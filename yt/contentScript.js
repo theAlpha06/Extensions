07cd45e58f436e5c52a3017a1f7b791d4fdaea2d
Shubham <95537249+theAlpha06@users.noreply.github.com>
2023-05-13 00:27:50 +0530

    added dark and light mode support

--- yt/contentScript.js
+++ yt/contentScript.js
@@ -1,24 +1,45 @@
 (() => {
+        const controlsImg = [
+            {
+                src: chrome.runtime.getURL("assets/loop.jpg"),
+                title: "Loop video"
+
+            },
+            {
+                src: chrome.runtime.getURL("assets/stop.png"),
+                title: "Stop video"
+            }
+        ]
         const newVideoLoaded = () => {
         const controlsDivExist = document.getElementsByClassName("controls-div")[0];
 
         if (!controlsDivExist) {
             const controlsDiv = document.createElement("div");
-            const toggleBtn = document.createElement('img');
-            controlsDiv.style.marginTop = '0';
-            toggleBtn.title = 'Hello';
-            toggleBtn.width = 30;
-            toggleBtn.src = chrome.runtime.getURL("assets/bookmark.png");
             controlsDiv.className = "controls-div";
-            controlsDiv.append(toggleBtn);
-            setTimeout(() => {
+            controlsDiv.style.display = "flex";
+            controlsDiv.style.alignItems = "center";
+            controlsDiv.style.justifyContent = "center";
+            controlsDiv.style.gap = "10px";
+
+            controlsImg.map((img) => {
+                const toggleBtn = document.createElement("img");
+                toggleBtn.title = img.title;
+                toggleBtn.width = 20;
+                toggleBtn.style.cursor = 'pointer';
+                toggleBtn.src = img.src;
+                if (window.matchMedia && 
+                    window.matchMedia('(prefers-color-scheme: dark)').matches) {
+                  toggleBtn.style.filter="invert(100%)";
+                }
+                controlsDiv.append(toggleBtn);
+            });
+
             const youtubeChrome = document.getElementsByClassName('ytd-watch-metadata')[0];
             if(!youtubeChrome) newVideoLoaded();
             else {
                 console.log(youtubeChrome)
                 youtubeChrome.insertBefore(controlsDiv, youtubeChrome.firstChild);
             }
-            }, 5000);
         }
     }
     newVideoLoaded();
