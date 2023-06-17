c2e6782de9434488ce82c7e7fd4357bb160d8508
Shubham <95537249+theAlpha06@users.noreply.github.com>
2023-06-17 17:21:57 +0530

    a simple chrome extension to debug your css

---
+++ Debugger/contentScript.js
@@ -0,0 +1,280 @@
+document.body.style.outline = "1px solid red";
+document.querySelectorAll("article").forEach((article) => {
+  article.style.outline = "1px solid green";
+});
+
+document.querySelectorAll("div").forEach((div) => {
+  div.style.outline = "1px solid blue";
+});
+
+document.querySelectorAll("p").forEach((p) => {
+  p.style.outline = "1px solid yellow";
+});
+
+document.querySelectorAll("h1").forEach((h1) => {
+  h1.style.outline = "1px solid orange";
+});
+
+document.querySelectorAll("h2").forEach((h2) => {
+  h2.style.outline = "1px solid purple";
+});
+
+document.querySelectorAll("h3").forEach((h3) => {
+  h3.style.outline = "1px solid pink";
+});
+
+document.querySelectorAll("h4").forEach((h4) => {
+  h4.style.outline = "1px solid brown";
+});
+
+document.querySelectorAll("h5").forEach((h5) => {
+  h5.style.outline = "1px solid grey";
+});
+
+document.querySelectorAll("h6").forEach((h6) => {
+  h6.style.outline = "1px solid black";
+});
+
+document.querySelectorAll("span").forEach((span) => {
+  span.style.outline = "1px solid blue";
+});
+
+document.querySelectorAll("a").forEach((a) => {
+  a.style.outline = "1px solid blue";
+});
+
+document.querySelectorAll("img").forEach((img) => {
+  img.style.outline = "1px solid blue";
+});
+
+document.querySelectorAll("button").forEach((button) => {
+  button.style.outline = "1px solid blue";
+});
+
+document.querySelectorAll("input").forEach((input) => {
+  input.style.outline = "1px solid blue";
+});
+
+document.querySelectorAll("label").forEach((label) => {
+  label.style.outline = "1px solid blue";
+});
+
+document.querySelectorAll("select").forEach((select) => {
+  select.style.outline = "1px solid blue";
+});
+
+document.querySelectorAll("textarea").forEach((textarea) => {
+  textarea.style.outline = "1px solid blue";
+});
+
+document.querySelectorAll("ul").forEach((ul) => {
+  ul.style.outline = "1px solid blue";
+});
+
+document.querySelectorAll("li").forEach((li) => {
+  li.style.outline = "1px solid blue";
+});
+
+document.querySelectorAll("nav").forEach((nav) => {
+  nav.style.outline = "1px solid blue";
+});
+
+document.querySelectorAll("header").forEach((header) => {
+  header.style.outline = "1px solid blue";
+});
+
+document.querySelectorAll("footer").forEach((footer) => {
+  footer.style.outline = "1px solid blue";
+});
+
+document.querySelectorAll("section").forEach((section) => {
+  section.style.outline = "1px solid blue";
+});
+
+document.querySelectorAll("aside").forEach((aside) => {
+  aside.style.outline = "1px solid blue";
+});
+
+document.querySelectorAll("main").forEach((main) => {
+  main.style.outline = "1px solid blue";
+});
+
+document.querySelectorAll("form").forEach((form) => {
+  form.style.outline = "1px solid blue";
+});
+
+document.querySelectorAll("table").forEach((table) => {
+  table.style.outline = "1px solid blue";
+});
+
+document.querySelectorAll("tr").forEach((tr) => {
+  tr.style.outline = "1px solid blue";
+});
+
+document.querySelectorAll("td").forEach((td) => {
+  td.style.outline = "1px solid blue";
+});
+
+document.querySelectorAll("th").forEach((th) => {
+  th.style.outline = "1px solid blue";
+});
+
+document.querySelectorAll("tbody").forEach((tbody) => {
+  tbody.style.outline = "1px solid blue";
+});
+
+document.querySelectorAll("thead").forEach((thead) => {
+  thead.style.outline = "1px solid blue";
+});
+
+document.querySelectorAll("tfoot").forEach((tfoot) => {
+  tfoot.style.outline = "1px solid blue";
+});
+
+document.querySelectorAll("caption").forEach((caption) => {
+  caption.style.outline = "1px solid blue";
+});
+
+document.querySelectorAll("video").forEach((video) => {
+  video.style.outline = "1px solid blue";
+});
+
+document.querySelectorAll("audio").forEach((audio) => {
+  audio.style.outline = "1px solid blue";
+});
+
+document.querySelectorAll("iframe").forEach((iframe) => {
+  iframe.style.outline = "1px solid blue";
+});
+
+document.querySelectorAll("canvas").forEach((canvas) => {
+  canvas.style.outline = "1px solid blue";
+});
+
+document.querySelectorAll("svg").forEach((svg) => {
+  svg.style.outline = "1px solid blue";
+});
+
+document.querySelectorAll("embed").forEach((embed) => {
+  embed.style.outline = "1px solid blue";
+});
+
+document.querySelectorAll("object").forEach((object) => {
+  object.style.outline = "1px solid blue";
+});
+
+document.querySelectorAll("param").forEach((param) => {
+  param.style.outline = "1px solid blue";
+});
+
+document.querySelectorAll("picture").forEach((picture) => {
+  picture.style.outline = "1px solid blue";
+});
+
+document.querySelectorAll("source").forEach((source) => {
+  source.style.outline = "1px solid blue";
+});
+
+document.querySelectorAll("track").forEach((track) => {
+  track.style.outline = "1px solid blue";
+});
+
+document.querySelectorAll("map").forEach((map) => {
+  map.style.outline = "1px solid blue";
+});
+
+document.querySelectorAll("area").forEach((area) => {
+  area.style.outline = "1px solid blue";
+});
+
+document.querySelectorAll("script").forEach((script) => {
+  script.style.outline = "1px solid blue";
+});
+
+document.querySelectorAll("noscript").forEach((noscript) => {
+  noscript.style.outline = "1px solid blue";
+});
+
+document.querySelectorAll("del").forEach((del) => {
+  del.style.outline = "1px solid blue";
+});
+
+document.querySelectorAll("ins").forEach((ins) => {
+  ins.style.outline = "1px solid blue";
+});
+
+document.querySelectorAll("caption").forEach((caption) => {
+  caption.style.outline = "1px solid blue";
+});
+
+document.querySelectorAll("col").forEach((col) => {
+  col.style.outline = "1px solid blue";
+});
+
+document.querySelectorAll("colgroup").forEach((colgroup) => {
+  colgroup.style.outline = "1px solid blue";
+});
+
+document.querySelectorAll("datalist").forEach((datalist) => {
+  datalist.style.outline = "1px solid blue";
+});
+
+document.querySelectorAll("details").forEach((details) => {
+  details.style.outline = "1px solid blue";
+});
+
+document.querySelectorAll("dialog").forEach((dialog) => {
+  dialog.style.outline = "1px solid blue";
+});
+
+document.querySelectorAll("wbr").forEach((wbr) => {
+  wbr.style.outline = "1px solid blue";
+});
+
+document.querySelectorAll("menu").forEach((menu) => {
+  menu.style.outline = "1px solid blue";
+});
+
+document.querySelectorAll("menuitem").forEach((menuitem) => {
+  menuitem.style.outline = "1px solid blue";
+});
+
+document.querySelectorAll("meter").forEach((meter) => {
+  meter.style.outline = "1px solid blue";
+});
+
+document.querySelectorAll("optgroup").forEach((optgroup) => {
+  optgroup.style.outline = "1px solid blue";
+});
+
+document.querySelectorAll("option").forEach((option) => {
+  option.style.outline = "1px solid blue";
+});
+
+document.querySelectorAll("output").forEach((output) => {
+  output.style.outline = "1px solid blue";
+});
+
+document.querySelectorAll("progress").forEach((progress) => {
+  progress.style.outline = "1px solid blue";
+});
+
+document.querySelectorAll("rp").forEach((rp) => {
+  rp.style.outline = "1px solid blue";
+});
+
+document.querySelectorAll("rt").forEach((rt) => {
+  rt.style.outline = "1px solid blue";
+});
+
+document.querySelectorAll("ruby").forEach((ruby) => {
+  ruby.style.outline = "1px solid blue";
+});
+
+document.querySelectorAll("summary").forEach((summary) => {
+  summary.style.outline = "1px solid blue";
+});
+
+document.querySelectorAll("time").forEach((time) => {
+  time.style.outline = "1px solid blue";
+});
