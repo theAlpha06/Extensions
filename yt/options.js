2b439103066ddf6ad6e9fb14b2402f6642209327
Shubham <95537249+theAlpha06@users.noreply.github.com>
2023-07-17 16:29:24 +0530

    chrome storage for option page

--- yt/options.js
+++ yt/options.js
@@ -1,26 +1,22 @@
-const togglePlayPause = document.getElementById("toggle_play_pause");
-const controlsPosition = document.getElementById("controls__position");
+let togglePlayPause = document.getElementById("toggle_play_pause");
+let controlsPosition = document.getElementById("controls__position");
+
+chrome.storage.sync.get(['togglePlayPause', 'controlsPosition']).then((result) => {
+  togglePlayPause.checked = result.togglePlayPause;
+  controlsPosition.value = result.controlsPosition;
+})
 
 const toggle = () => {
   if (togglePlayPause.checked) {
-    chrome.storage.sync.set({ togglePlayPause: true }).then(() => {
-      console.log("Value is set");
-    });
-    chrome.storage.sync.get(["togglePlayPause"]).then((result) => {
-      console.log("Value currently is " + result.togglePlayPause);
-    });
+    chrome.storage.sync.set({ togglePlayPause: true });
   } else {
-    chrome.storage.sync.set({ togglePlayPause: false }).then(() => {
-      console.log("Value is reset");
-    });
-    chrome.storage.sync.get(["togglePlayPause"]).then((result) => {
-      console.log("Value currently is " + result.togglePlayPause);
-    });
+    chrome.storage.sync.set({ togglePlayPause: false });
   }
 };
-togglePlayPause.addEventListener("click", toggle);
 
 const controlPosition = () => {
   chrome.storage.sync.set({ controlsPosition: controlsPosition.value });
 }
+
+togglePlayPause.addEventListener("click", toggle);
 controlsPosition.addEventListener("change", controlPosition);
